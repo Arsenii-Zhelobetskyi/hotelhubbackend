0f375356d93f38fcd05851acdf74a9fcf7f75b9f
Arsenii-Zhelobetskyi <arsbrotherplay@gmail.com>
2023-11-19 14:27:14 +0200

    new api`s for comments pagination 🎩

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -4,12 +4,33 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const router = express.Router();
 
+router.get("/count", async (req, res) => {
+  try {
+    const id = parseInt(req.query.id);
+    const type = req.query.type;
+    const quantity = await prisma[`${type}s_comments`].count({
+      where: {
+        [`${type}_id`]: id,
+      },
+    });
+    res.json({ quantity });
+  } catch (err) {
+    console.error(err);
+    res.json(err);
+  }
+});
+
 router.get("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
   const type = req.query.type;
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
+  const skip = (page - 1) * limit;
   const data = await prisma[`${type}s_comments`].findMany({
-    take: 5,
+    skip: skip,
+    take: limit,
     where: {
-      [`${type}_id`]: parseInt(req.params.id),
+      [`${type}_id`]: id,
     },
   });
   res.json(data);
@@ -19,7 +40,6 @@ router.post("/create", async (req, res) => {
   try {
     const type = req.query.type;
     const { title, body, rating, id } = req.body;
-    console.log(req.body);
     const newComment = await prisma[`${type}s_comments`].create({
       data: {
         title,
@@ -34,4 +54,18 @@ router.post("/create", async (req, res) => {
     res.json(err);
   }
 });
+
+router.post("/sortBy", async (req, res) => {
+  try {
+    const type = req.query.type;
+    const { title, body, rating, id } = req.body;
+    console.log(req.body);
+
+    res.json(newComment);
+  } catch (err) {
+    console.error(err);
+    res.json(err);
+  }
+});
+
 module.exports = router;
