fb1495c6020a6139b7ac332164efb2a2bf8cafc6
Arsenii-Zhelobetskyi <arsbrotherplay@gmail.com>
2023-11-22 00:06:12 +0200

    catalog now has working pagination

--- src/routes/hotels.js
+++ src/routes/hotels.js
@@ -1,51 +1,28 @@
-/* const { PrismaClient } = require("@prisma/client");
-
-const prisma = new PrismaClient();
-
-class HotelService {
-  //класи запропонував ДЖПТІШКА, по суті хз можна відразу робить у index js різні шляхи  й там долбіть прізму, але хз
-  async createHotel(data) {
-    const hotel = await prisma.hotel.create({
-      data,
-    });
-    return hotel;
-  }
-
-  async getHotelById(id) {
-    const hotel = await prisma.hotel.findUnique({
-      where: { id: id },
-    });
-    return hotel;
-  }
-
-  async getPhotoHotelById(id) {
-    const hotel = await prisma.hotel.findUnique({
-      where: { id: id },
-    });
-    if (hotel && hotel.photo) {
-      return hotel.photo;
-    } else {
-      return null;
-    }
-  }
-
-  async getAllHotels() {
-    const hotels = await prisma.hotel.findMany();
-    return hotels;
-  }
-}
-
-const hotelInstance = new HotelService();
-
-module.exports = hotelInstance; */
-
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 const router = express.Router();
+
+router.get("/count", async (req, res) => {
+  try {
+    const quantity = await prisma.hotel.count();
+    res.json({ quantity });
+  } catch (err) {
+    console.error(err);
+    res.json(err);
+  }
+});
+
 router.get("/", async (req, res) => {
-  const data = await prisma.hotel.findMany();
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
+  const skip = (page - 1) * limit;
+
+  const data = await prisma.hotel.findMany({
+    skip: skip,
+    take: limit,
+  });
 
   res.json(data);
 });
--- src/routes/houses.js
+++ src/routes/houses.js
@@ -32,9 +32,24 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 const router = express.Router();
-
+router.get("/count", async (req, res) => {
+  try {
+    const quantity = await prisma.house.count();
+    res.json({ quantity });
+  } catch (err) {
+    console.error(err);
+    res.json(err);
+  }
+});
 router.get("/", async (req, res) => {
-  const data = await prisma.house.findMany();
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
+  const skip = (page - 1) * limit;
+
+  const data = await prisma.house.findMany({
+    skip: skip,
+    take: limit,
+  });
   res.json(data);
 });
 router.get("/house/:id", async (req, res) => {
