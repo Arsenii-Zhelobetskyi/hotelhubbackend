759d36a201b69ed046418c4cc1355ea10b70c66b
Arsenii-Zhelobetskyi <arsbrotherplay@gmail.com>
2023-11-22 11:40:31 +0200

    working on orderNow

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -1,56 +1,8 @@
-/* const { PrismaClient } = require("@prisma/client");
-
-const prisma = new PrismaClient();
-
-class RoomService {
-  
-  async createRoom(data) {
-    const room = await prisma.room.create({
-      data,
-    });
-    return room;
-  }
-
-  // Приклад методу в класі, який працює з кімнатами
-async getOccupiedPlacesByHotel() {
-
-  const occupiedRooms = await prisma.room.groupBy({
-    by: ['hotel_id'],
-    _sum: {
-      placeN: true,
-    },
-    where: {
-      status: 'occupied',
-    },
-  });
-    const occupiedPlacesByHotel = occupiedRooms.map((roomGroup) => ({
-  hotelId: roomGroup.hotel_id,
-  totalPlaces: roomGroup._sum.placeN || 0,
-}));
-    console.log(occupiedPlacesByHotel);
-  return occupiedPlacesByHotel;
-}
-
-  async getAllRoom() {
-    const rooms = await prisma.room.findMany();
-    return rooms;
-  }
-}
-
-const roomInstance = new RoomService();
-
-module.exports = roomInstance;
- */
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 const router = express.Router();
-
-router.get("/", async (req, res) => {
-  const data = await prisma.room.findMany();
-  res.json(data);
-});
 
 router.get("/occupiedPlacesByHotel", async (req, res) => {
   const occupiedRooms = await prisma.room.groupBy({
@@ -76,4 +28,10 @@ router.get("/create", async (req, res) => {
   res.json(data);
 });
 
+router.get("/:id", async (req, res) => {
+  const data = await prisma.room.findMany({
+    where: { hotel_id: parseInt(req.params.id) },
+  });
+  res.json(data);
+});
 module.exports = router;
